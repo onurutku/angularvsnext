0e2ff8de40d49f4da2a12070fac19a838c505403
onurutku <topaloglu08@gmail.com>
2023-05-10 16:18:45 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'AngularVSNext';
+  photos: any = [];
+  constructor(private readonly http: HttpClient) {
+    this.http
+      .get('https://jsonplaceholder.typicode.com/photos')
+      .subscribe((photos: any) => {
+        const slicedArray = photos.slice(0, 100);
+        this.photos = slicedArray;
+      });
+  }
 }
